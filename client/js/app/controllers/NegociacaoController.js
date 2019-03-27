ba002b51d07ab56f1514a726a5ad5a0f534e0eff
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-03-27 15:04:56 -0300

    aula 4.1 - Idéia inicial de como evitar importar o mesmo item mais de uma vez

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -72,6 +72,11 @@ class NegociacaoController {
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
+            .the(negociacoes => 
+                    negociacoes.filter(negociacao => 
+                        this._listaNegociacoes.negociacoes.indexOf(negociacao) == -1
+                    )
+            )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
                 this._mensagem.texto = 'Negociações do período importadas'   
