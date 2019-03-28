44046e8962d578d50b4c861c18b5cf2d446dda8d
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-03-28 15:41:30 -0300

    aula 5.1 - Trocando a implementação "na mão" do get pela função fetch do ES6

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,31 +1,32 @@
 class HttpService {
     
-    get(url) {
-        
-        return new Promise((resolve, reject) => {
-            
+    _handleErrors(res) {
+        if (!res.ok) throw new Error(res.statusText);
+        return res;
+    }
 
-            let xhr = new XMLHttpRequest();
-            
-            xhr.open('GET', url);
-            
-            xhr.onreadystatechange = () => {
-                    
-                if(xhr.readyState == 4) {
-                    
-                    if(xhr.status == 200) {   
-                        
-                        resolve(JSON.parse(xhr.responseText));  
-                    } else {
-                        
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            
-            xhr.send();
-             
-            
-        });
+    get(url) {
+
+        return fetch(url)
+                    .then(res => this._handleErrors(res))
+                    .then(res => res.json());
+        
+        // O ES6 implementou um método muito mais fácil (acima) de pegar conteúdo de páginas.
+        // esse código abaixo está sendo mantido comentado apenas para fins educativos.
+        //
+        // return new Promise((resolve, reject) => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open('GET', url);
+        //     xhr.onreadystatechange = () => {
+        //         if(xhr.readyState == 4) {
+        //             if(xhr.status == 200) {   
+        //                 resolve(JSON.parse(xhr.responseText));  
+        //             } else {
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     };
+        //     xhr.send();
+        // });
     }
 }
